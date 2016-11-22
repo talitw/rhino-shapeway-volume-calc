96ede0da78499895481bc952dfe836286c4d7e21
Tali Weissberger <tali@drawingpost.com>
2016-11-21 22:41:14 -0800

    Simplify material calculation

--- ShapewaysVolumeCalc_cmd.py
+++ ShapewaysVolumeCalc_cmd.py
@@ -1,23 +1,18 @@
 import rhinoscriptsyntax as rs
 
 __commandname__ = "ShapewaysVolumeCalc"
+def compute_from_material(material_name, handling_cost, volume_cost, volume):
+    cost = handling_cost + volume_cost * volume
+    print "{}: ${} + ${} * {:0.2f} cm3 = ${:0.2f}".format(material_name, handling_cost, volume_cost, volume, cost)
 
 def RunCommand( is_interactive ):
     obj = rs.GetObject("Select mesh", rs.filter.mesh )
     if obj and rs.IsMeshClosed(obj):
         volume = rs.MeshVolume(obj)[1]/1000
-        gold_14k = volume * 600 + 50
-        gold_18k = volume * 800 + 100
-        platinum = volume * 1750 + 100
-        silver_polished = volume * 20 + 35
-        silver_raw = volume * 20 + 30
-        bronze_or_brass_polished = volume * 18 + 20
-        bronze_or_brass_raw = volume * 16 + 10
-
-        print "Gold 14k: $50 + $600 * {:0.2f} cm3 = ${:0.2f}".format(volume, gold_14k)
-        print "Gold 18k: $100 + $800 * {:0.2f} cm3 = ${:0.2f}".format(volume, gold_18k)
-        print "Platinum: $100 + $1750 * {:0.2f} cm3 = ${:0.2f}".format(volume, platinum)
-        print "Silver -Polished: $35 + $20 * {:0.2f} cm3 = ${:0.2f}".format(volume, silver_polished)
-        print "Silver -Raw: $30 + $20 * {:0.2f} cm3 = ${:0.2f}".format(volume, silver_raw)
-        print "Bronze/Brass -Polished: $20 + $18 * {:0.2f} cm3 = ${:0.2f}".format(volume, bronze_or_brass_polished)
-        print "Bronze/Brass -Raw $10 + $16 * {:0.2f} cm3 = ${:0.2f}".format(volume, bronze_or_brass_raw)
+        compute_from_material("Gold 14k", 50, 600, volume)
+        compute_from_material("Gold 18k", 100, 800, volume)
+        compute_from_material("Platinum", 100, 1750, volume)
+        compute_from_material("Silver Polished", 35, 20, volume)
+        compute_from_material("Silver -Raw", 30, 20, volume)
+        compute_from_material("Bronze/Brass -Polished", 20, 18, volume)
+        compute_from_material("Bronze/Brass -Raw", 10, 16, volume)
